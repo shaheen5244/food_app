0446f92189b2433819baf5c774a3fad23c7a5f5b
shaheen <shaheenmhd2024@gmail.com>
2024-09-01 20:09:26 +0530

    settings

--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -1,21 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:food_app/components/my_tile_drawer.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 100),
-            child: Icon(Icons.lock_open_rounded,
-            size: 80,
-            color: Theme.of(context).colorScheme.inversePrimary,),
+    return Padding(
+      padding: const EdgeInsets.only(left: 25),
+      child: Drawer(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 100),
+              child: Icon(
+                Icons.lock_open_rounded,
+                size: 80,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Divider(
+                color: Theme.of(context).colorScheme.secondary,
+              ),
+            ),
+            MyDrawerTile(
+              text: "HOME",
+              icon: Icons.home,
+              onTap: () {},
+            ),
+            MyDrawerTile(
+              text: "SETTINGS",
+              icon: Icons.settings,
+              onTap: () {},
+            ),
+
+            const Spacer(),
+
+            MyDrawerTile(
+            text: "LOGOUT",
+            icon: Icons.logout,
+            onTap: () {},
           )
-        ],
+          ],
+        ),
       ),
     );
   }
